a9e7e7fffcefa0e4e893463a256eb02c193cb236
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2025-01-26 10:42:23 +0000

    currying solution

--- currying.ts
+++ currying.ts
@@ -12,40 +12,45 @@ const accelaration = 2;
 const maxVelocity = 10;
 
 function updatePlayer(player: Player, inputData: Vector2D): Player {
-  const newDirection = normalizeVector(inputData);
-  const destination = scaleVector(newDirection, maxVelocity);
-  const updatedVelocity = moveTowards(player.velocity, destination, accelaration);
-
-  return {
-    ...player,
-    velocity: updatedVelocity,
-    position: addVectors(player.position, updatedVelocity)
-  };
+  return _.pipe(
+    inputData,
+    normalizeVector,
+    scaleVector(maxVelocity),
+    moveTowards(player.velocity, accelaration),
+    updateVelocity(player),
+    movePlayer
+  );
 }
+
+const updateVelocity = (player: Player) => (velocity: Vector2D): Player => ({
+  ...player,
+  velocity
+});
+
+const movePlayer = (player: Player): Player => ({
+  ...player,
+  position: addVectors(player.position, player.velocity)
+});
 
 function vectorLength(vector: Vector2D) {
   return Math.sqrt(vector.x ** 2 + vector.y ** 2);
 }
 
-function scaleVector(vector: Vector2D, by: number): Vector2D {
-  return {
-    x: vector.x * by,
-    y: vector.y * by
-  };
-}
+const scaleVector = (by: number) => (vector: Vector2D): Vector2D => ({
+  x: vector.x * by,
+  y: vector.y * by
+});
 
 function normalizeVector(vector: Vector2D): Vector2D {
   const length = vectorLength(vector);
 
-  return scaleVector(vector, 1 / length);
+  return scaleVector(1 / length)(vector);
 }
 
-function moveTowards(vector: Vector2D, destination: Vector2D, step: number): Vector2D {
-  return {
-    x: vector.x > destination.x ? Math.max(vector.x - step, destination.x) : Math.min(vector.x + step, destination.x),
-    y: vector.y > destination.y ? Math.max(vector.y - step, destination.y) : Math.min(vector.y + step, destination.y)
-  };
-}
+const moveTowards = (vector: Vector2D, step: number) => (destination: Vector2D): Vector2D => ({
+  x: vector.x > destination.x ? Math.max(vector.x - step, destination.x) : Math.min(vector.x + step, destination.x),
+  y: vector.y > destination.y ? Math.max(vector.y - step, destination.y) : Math.min(vector.y + step, destination.y)
+});
 
 function addVectors(vector1: Vector2D, vector2: Vector2D): Vector2D {
   return {
@@ -60,14 +65,25 @@ function addVectors(vector1: Vector2D, vector2: Vector2D): Vector2D {
 type Validator = (value: unknown) => boolean;
 
 const isAnything = (value: unknown) => true;
-const isString = isAnything;
-const isNumber = isAnything;
-const isBoolean = isAnything;
-const either = (...validators: Validator[]) => isAnything;
-const isPrimitive = isAnything;
-const isArrayOf = (validator: Validator) => isAnything;
-const hasShape = (validators: Record<PropertyKey, Validator>) => isAnything;
-const optionally = (validator: Validator) => isAnything;
+const isString = (value: unknown) => typeof value === "string";
+const isNumber = (value: unknown) => typeof value === "number";
+const isBoolean = (value: unknown) => typeof value === "boolean";
+const either = (...validators: Validator[]) => (value: unknown) => validators.some((validator) => validator(value));
+const isPrimitive = either(isString, isNumber, isBoolean);
+const isArrayOf = (validator: Validator) => (value: unknown) => Array.isArray(value) && value.every(validator);
+
+const hasShape = (validators: Record<PropertyKey, Validator>) => (value: unknown) => {
+  if (typeof value !== "object" || value == null) {
+    return false;
+  }
+
+  const allValidatorsPass = Object.entries(validators).every(([key, validator]) => validator(value[key as keyof typeof value]));
+  const allKeysPass = Object.keys(value).every((key) => key in validators);
+
+  return allKeysPass && allValidatorsPass;
+};
+
+const optionally = (validator: Validator) => (value: unknown) => value == null || validator(value);
 
 const isValidResponse = hasShape({
   id: isString,
